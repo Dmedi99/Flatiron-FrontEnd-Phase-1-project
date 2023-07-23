485641def89e7964149ebd6448c2a33f60f81fc3
David <davidmedina0907@gmail.com>
2023-07-23 13:02:25 -0700

    added function to handle Data fetched from API

--- main.js
+++ main.js
@@ -1,64 +1,44 @@
-// Function to display data in the HTML file
-function displayData(boardGames) {
-    const boardGamesListElement = document.getElementById('boardGamesList');
-    const ul = document.createElement('ul');
-  
-    boardGames.forEach((boardGame) => {
-      const li = document.createElement('li');
-  
-      // Create an image element for the thumbnail
-      const thumbnailImg = document.createElement('img');
-      thumbnailImg.src = boardGame.thumbnail;
-      thumbnailImg.alt = boardGame.name;
-      li.appendChild(thumbnailImg);
-  
-      // Create a span element for the year published
-      const yearPublishedSpan = document.createElement('span');
-      yearPublishedSpan.textContent = `Year Published: ${boardGame.yearPublished}`;
-      li.appendChild(yearPublishedSpan);
-  
-      // Create a span element for the name
-      const nameSpan = document.createElement('span');
-      nameSpan.textContent = `Name: ${boardGame.name}`;
-      li.appendChild(nameSpan);
-  
-      ul.appendChild(li);
-    });
-  
-    boardGamesListElement.appendChild(ul);
-  }
+// link for the API 
+const endpoint = 'https://boardgamegeek.com/xmlapi2/hot?boardgame';
 
-  
- // Function to fetch data from the API and handle errors
-function fetchData() {
-    const apiUrl = 'https://boardgamegeek.com/xmlapi2/hot?videogame';
-  
-    return fetch(apiUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
+//Gets what's inside the ul in the HTMl to manipulate it
+const boardGamesListElement = document.getElementById('boardGamesList');
+
+//Function to fetch the information from the API
+function fetchBoardGames() {
+    return fetch(endpoint)
+        .then((response) => response.text())
+        .then((data) => {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(data, 'text/xml');
+            const items = xmlDoc.getElementsByTagName('item');
+            return Array.from(items).map((item) => ({
+                name: item.getElementsByTagName('name')[0].getAttribute('value'),
+                yearPublished: item.getElementsByTagName('yearpublished')[0].getAttribute('value')
+            }));
+        })
+        .catch((error) => {
+            console.error('Error fetching board games:', error);
+            return [];
+        });
+}
+
+//Function that handles the data fetched from the API to display it to the DOM 
+function displayBoardGames() {
+    fetchBoardGames().then((games) => {
+        if (games && games.length > 0) {
+            games.forEach((game) => {
+                const listItem = document.createElement('li');
+                listItem.innerText = `${game.name} (${game.yearPublished})`;
+                boardGamesListElement.appendChild(listItem);
+            });
+        } else {
+            const listItem = document.createElement('li');
+            listItem.innerText = 'No hot board games found.';
+            boardGamesListElement.appendChild(listItem);
         }
-        return response.text();
-      })
-      .then((data) => {
-        const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(data, 'text/xml');
-        const items = xmlDoc.getElementsByTagName('item');
-  
-        const boardGames = [];
-        for (let i = 0; i < items.length; i++) {
-          const name = items[i].getElementsByTagName('name')[0].getAttribute('value');
-          const thumbnail = items[i].getElementsByTagName('thumbnail').getAttribute('value'); // Get the thumbnail URL from the attribute
-          const yearPublished = items[i].getElementsByTagName('yearpublished')[0]?.getAttribute('value');
-          boardGames.push({ name, thumbnail, yearPublished });
-        }
-        return boardGames;
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-        return [];
-      });
-  }
-   
-  
-  
+    });
+}
+
+//Calls the function to display data, without this it'll be a blank DOM 
+displayBoardGames();
