b581501bc929756cebd4fd792df5324eb7b9ea93
David <davidmedina0907@gmail.com>
2023-07-23 14:47:07 -0700

    added a function to filter games and seach input

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-// link for the API 
-const endpoint = 'https://boardgamegeek.com/xmlapi2/hot?boardgame';
+//variable that holds the API link 
+const API = 'https://boardgamegeek.com/xmlapi2/hot?boardgame';
 
-//Gets what's inside the ul in the HTMl to manipulate it
+//variables that search the HTML for specific Ids 
 const boardGamesListElement = document.getElementById('boardGamesList');
+const searchForm = document.getElementById('searchForm');
+const searchNameInput = document.getElementById('searchName');
 
-//Function to fetch the information from the API
+
+//function that fetches the data from the API and catches error if any 
 function fetchBoardGames() {
-    return fetch(endpoint)
+    return fetch(API)
         .then((response) => response.text())
         .then((data) => {
             const parser = new DOMParser();
@@ -18,27 +21,30 @@ function fetchBoardGames() {
             }));
         })
         .catch((error) => {
-            console.error('Error fetching board games:', error);
+            console.error('Error fetching board games from API', error);
             return [];
         });
 }
 
-//Function that handles the data fetched from the API to display it to the DOM 
-function displayBoardGames() {
-    fetchBoardGames().then((games) => {
-        if (games && games.length > 0) {
-            games.forEach((game) => {
-                const listItem = document.createElement('li');
-                listItem.innerText = `${game.name} (${game.yearPublished})`;
-                boardGamesListElement.appendChild(listItem);
-            });
-        } else {
+//Function that displays the data from the fetch onto the DOM
+function displayBoardGames(games) {
+    boardGamesListElement.innerHTML = '';
+    if (games && games.length > 0) {
+        games.forEach((game) => {
             const listItem = document.createElement('li');
-            listItem.innerText = 'No hot board games found.';
+            listItem.innerText = `${game.name} (${game.yearPublished})`;
             boardGamesListElement.appendChild(listItem);
-        }
-    });
+        });
+    } else {
+        const listItem = document.createElement('li');
+        listItem.innerText = 'No hot board games found.';
+        boardGamesListElement.appendChild(listItem);
+    }
 }
 
-//Calls the function to display data, without this it'll be a blank DOM 
-displayBoardGames();
+//Function that takes the array of the 'games' and 'searchTerm as inputs and returns a new array containing the matching search criteria 
+function filterBoardGames(games, searchTerm) {
+    return games.filter((game) => game.name.toLowerCase().includes(searchTerm.toLowerCase()));
+}
+
+
